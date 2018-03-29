7a8fc82fc29e128227edd8575350c57b69a59b3c
TREND\jenny_liu <jenny_liu@trend.com.tw>
2018-03-29 16:40:11 +0800

    add Touppser and WindowsFormsApplication1

---
+++ web_api/Touppser/Toupper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Touppser
+{
+    public class c1
+    {
+        public string Touppser(string word)//方法名稱
+        {
+            return word.ToUpper();
+        }
+    }
+}
---
+++ web_api/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Touppser;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        } 
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            c1 x = new c1();//用new 建立類別實體
+            textBox2.Text = x.Touppser(textBox2.Text);//使用類別的方法
+        }
+    }
+}
