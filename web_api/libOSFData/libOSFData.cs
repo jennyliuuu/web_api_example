447493e289a979494dbb0dfbef7d26ff055685f6
TREND\jenny_liu <jenny_liu@trend.com.tw>
2018-05-09 18:34:48 +0800

    modify libOSFData

--- web_api/libOSFData/libOSFData.cs
+++ web_api/libOSFData/libOSFData.cs
@@ -6,14 +6,18 @@ using System.Threading.Tasks;
 
 namespace libOSFData
 {
+    public class Data
+    {
+    }
+    public class SystemCallData
+    {
+        public string product_id { get; set; }
+        public Data data { get; set; }
+        public string cookie { get; set; }
+    }
     public class OSFRequestSystemCall
     {
         public string system_call_id { get; set; }
-        public OSFSystemCallData system_call_data { get; set; }
-    }
-    public class OSFSystemCallData
-    {
-        public string product_id { get; set; }
-        public object data { get; set; }
+        public SystemCallData system_call_data { get; set; }
     }
 }
